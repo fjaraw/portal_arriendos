88eef2829e635226e0c83510ad1a9ebe6b07e70a
Fernando Jara <fjaraw@hotmail.com>
2024-07-08 22:41:37 -0400

    Función para agregar usuarios.

---
+++ main/management/commands/test.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from main.services import *
+# se ejecuta: python manage.py test
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        #prueba de funciones desde services.py
+        print('Testeo de programa')
+        #crear_user(123456789, 'John', 'Doe', 'johndoe@xyz.com', '1234', 'Hill St. 1010', '+56911223344')
+        #crear_user(123456780, 'Jane', 'Doe', 'janedoe@xyz.com', '1234', 'Hill St. 1010', '+56911223344')
+        print('Operación realizada.')
--- main/services.py
+++ main/services.py
@@ -1,12 +1,26 @@
+from main.models import Comuna, Inmueble, UserProfile
+from django.contrib.auth.models import User
+
 def crear_inmueble(*args):
     pass
 def editar_inmueble(*args):
     pass
 def eliminar_inmueble(inmueble_id):
     pass
-def crear_user(*args):
-    pass
+def crear_user(rut, first_name, last_name, email, password, direccion, telefono):
+    user = User.objects.create_user(
+        username=rut,
+        first_name = first_name,
+        last_name = last_name,
+        email = email,
+        password = password
+    )
+    UserProfile.objects.create(
+        direccion = direccion,
+        telefono = telefono,
+        user = user
+        )
 def editar_user(*args):
     pass
-def eliminar_user(*args):
+def eliminar_user(user_id):
     pass
