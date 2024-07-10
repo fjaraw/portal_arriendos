4464a45a76084d88d7a27cde0af06ae2f28820bc
Fernando Jara <fjaraw@hotmail.com>
2024-07-09 20:49:57 -0400

    Mejoras en funcion crear usuario.

--- main/management/commands/test.py
+++ main/management/commands/test.py
@@ -6,6 +6,6 @@ class Command(BaseCommand):
         #prueba de funciones desde services.py
         print('Testeo de programa')
         #crear_user(123456789, 'John', 'Doe', 'johndoe@xyz.com', '1234', 'Hill St. 1010', '+56911223344')
-        #crear_user(123456780, 'Jane', 'Doe', 'janedoe@xyz.com', '1234', 'Hill St. 1010', '+56911223344')
-        eliminar_user('123456780')
+        #crear_user(12345678-0, 'Jane', 'Doe', 'janedoe@xyz.com', '1234', '1234', 'Hill St. 1010', '+56911223344')
+        #eliminar_user('123456780')
         print('Operación realizada.')
--- main/services.py
+++ main/services.py
@@ -1,5 +1,6 @@
 from main.models import Comuna, Inmueble, UserProfile
 from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 
 def crear_inmueble(*args):
     pass
@@ -7,19 +8,25 @@ def editar_inmueble(*args):
     pass
 def eliminar_inmueble(inmueble_id):
     pass
-def crear_user(rut, first_name, last_name, email, password, direccion, telefono):
-    user = User.objects.create_user(
-        username=rut,
-        first_name = first_name,
-        last_name = last_name,
-        email = email,
-        password = password
-    )
+def crear_user(rut, first_name, last_name, email, password, pass_confirm, direccion, telefono=None) -> list[bool, str]:
+    if password != pass_confirm:
+        return False, 'Contraseñas no coinciden.'
+    try:
+        user = User.objects.create_user(
+            username=rut,
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            password = password
+        )
+    except IntegrityError:
+        return False, 'Rut Duplicado.'
     UserProfile.objects.create(
         direccion = direccion,
         telefono = telefono,
         user = user
         )
+    return True
 def editar_user(*args):
     pass
 def eliminar_user(rut):
