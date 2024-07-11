9e0a37d1d72318043ec52e3935b0ecc3e2b5cf06
Fernando Jara <fjaraw@hotmail.com>
2024-07-11 19:37:51 -0400

    Carga de inmuebles desde tabla CSV.

--- main/management/commands/load_inmuebles.py
+++ main/management/commands/load_inmuebles.py
@@ -1,14 +1,16 @@
 from django.core.management.base import BaseCommand
 import csv
-from main.models import UserProfile
-# se ejecuta: python manage.py load_comunas
+from main.services import crear_inmueble
+from main.models import Comuna
+# se ejecuta: python manage.py load_inmuebles
 # se usa solo cuando recién creamos nuestra base de datos
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         archivo = open('data/inmuebles.csv', encoding='utf-8')
         reader = csv.reader(archivo, delimiter=';')
         #avanza una línea para omitir encabezados de la tabla
-        #next(reader)
-        #for fila in reader:
-            #Comuna.objects.create(nombre=fila[0],cod=fila[1],region_id=fila[3])
-            #print(fila[0])
+        next(reader)
+        for fila in reader:
+            #crear_inmueble('Casa Tradicional', 'Casa de dos pisos en sector regional', 120, 2000, 1, 4, 2, 'Eliodoro Yañez 1642', 'casa', 11000000, 14101, '123456789')
+            crear_inmueble(fila[0],fila[1],fila[2],fila[3],fila[4],fila[5],fila[6],fila[7],fila[8],fila[9],fila[10],fila[11])
+            #print(f'{fila[0]} - {fila[1]} - {fila[2]} - {fila[3]} - {fila[4]} - {fila[5]} - {fila[6]} - {fila[7]} - {fila[8]} - {fila[9]} - {fila[10]} - {fila[11]}')
